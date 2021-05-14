2237c46f90d448d8a86f0504cdfe816e9ce6051e
其妙 <hling51325@163.com>
2021-05-14 18:37:36 +0800

    fix

--- app.ts
+++ app.ts
@@ -3,19 +3,13 @@ import Koa from 'koa'
 import config from 'config'
 import router from './lib/route'
 import wss from './lib/service/wss'
-import http from 'http'
 
 // 注册事件
 import './lib/service/socket-event'
 
-http.createServer()
-const PORT = config.get('PORT')
+const PORT = config.get('PORT') || 3000
 
 const app = new Koa()
-
-// wss connect
-// wss(app)
-
 app
   .use(router.routes())
   .use(router.allowedMethods())
@@ -25,5 +19,4 @@ const server = app.listen(PORT)
 wss.init(server)
 
 console.log(`listening port: ${PORT} ...`)
-
 export default app
