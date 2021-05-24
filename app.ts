faff56f32fd9982262a257f29675068a5cd03ee3
其妙 <hling51325@163.com>
2021-05-24 21:06:19 +0800

    wip

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import bodyParser from 'koa-bodyparser'
 import router from './lib/route'
 import wss from './lib/service/wss'
 import global from './lib/service/global'
-
+import serve from 'koa-static'
 // 注册事件
 import './lib/service/socket-event'
 
@@ -13,12 +13,17 @@ const PORT = global.get('PORT') || 3000
 
 const app = new Koa()
 
+
 app.use(cors({
   origin: '*',
   allowMethods: ['POST', 'OPTIONS', 'PUT', 'HEAD', 'DELETE', 'PATCH']
 }))
 app.use(bodyParser())
 app.use(logger())
+app.use(serve(__dirname + '/node_modules/bilibili-danmaku-page/dist', {
+  maxage: 60 * 1000,
+  defer: false,
+}))
 
 app.use(async (ctx, next) => {
   try {
