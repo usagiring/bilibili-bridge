adcbd60034034f27bf87aab0bd406711e8c5eac4
hling51325 <hling51325@163.com>
2026-06-16 11:33:24 +0800

    wip

--- lib/service/db.ts
+++ lib/service/db.ts
@@ -1,14 +1,9 @@
 /**
  * Drizzle + SQLite 数据库实例
- * 使用 better-sqlite3 驱动
  */
-import { drizzle } from 'drizzle-orm/better-sqlite3'
-import Database from 'better-sqlite3'
 import path from 'path'
+import { drizzle } from 'drizzle-orm/node-sqlite'
 
 const dbPath = process.env.DB_PATH || path.join(process.cwd(), 'db.sqlite')
 console.log(dbPath)
-const sqlite = new Database(dbPath)
-sqlite.pragma('journal_mode = WAL')
-
-export const db = drizzle(sqlite)
+export const db = drizzle(dbPath)
